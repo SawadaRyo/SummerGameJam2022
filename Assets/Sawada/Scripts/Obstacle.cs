3efb9bc5afccfc547c3c1b33479fa6e5250551fc
SR21C <rysawad@gmail.com>
2022-08-18 15:37:32 +0900

    スクリプトフォルダ追加

--- Assets/Sawada/Scripts/Obstacle.cs
+++ Assets/Sawada/Scripts/Obstacle.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Obstacle : ItemBase
 {
+    [SerializeField] float _debufValue = 5f;
     public override void ItemEffect()
     {
         //ProgressUIManager.Instance.ChangePlayerProgress()
