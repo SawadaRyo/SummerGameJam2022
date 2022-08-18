7f36bd3a456710a2b8fa0bde8977c7cdf932489e
ryuoomiya <oomiyaryu@gmail.com>
2022-08-18 16:02:11 +0900

    穴に落ちたらゲームオーバーを追加しました

--- Assets/Oomiya/FallHole.cs
+++ Assets/Oomiya/FallHole.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FallHole : MonoBehaviour
 {
-    Vector2 _normalPos = new Vector2(0,0);
-    Vector2 _playerPos;
+    [Tooltip("基準値")] Vector2 _normalPos = new Vector2(0,0);
+    [Tooltip("プレイヤー座標")]Vector2 _playerPos;
     void Start()
     {
-        _playerPos = GetComponent<Transform>().position;
+        _playerPos = GameObject.Find("Player").GetComponent<Transform>().position;
     }
 
     void Update()
     {
-        
+        Vector2 diff = _playerPos - _normalPos;
+
+        //if(diff.magnitude < -4.5 )
+        //{
+        //    SceneManager.LoadScene("GameOver");       //穴に落ちたらゲームオーバー
+        //}
     }
 }
