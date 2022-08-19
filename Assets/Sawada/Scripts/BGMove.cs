4987456c17c7e0b9431262ecf942b84a4b1b0a1b
SR21C <rysawad@gmail.com>
2022-08-19 15:20:41 +0900

    プレハブ追加

---
+++ Assets/Sawada/Scripts/BGMove.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMove : GroundMove
+{
+    [SerializeField] Transform _basePos = default;
+    [SerializeField] Transform _setPos = default;
+    
+    void ResetPos()
+    {
+        float posX = this.transform.position.x;
+        if (posX -_basePos.position.x <= -60)
+        {
+            this.transform.position = _setPos.position;
+        }
+    }
+}
