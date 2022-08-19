34a70d819f9e13ededdda8680f4624d8a434ac19
Kaito913ps <86725129+Kaito913ps@users.noreply.github.com>
2022-08-19 13:40:42 +0900

    ランキングTextを構築した。

---
+++ Assets/Oohata/Scripts/Ranking.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class Ranking : MonoBehaviour
+{
+    private int[] _top5 = new int[5] { 5, 3, 7, 9, 40 };
+    public int _Score = 3;
+    private int count = 0;
+    [SerializeField]
+    Text[] _Rankin;
+
+
+    void Start()
+    {
+        var _rankingList = _top5.ToList<int>();
+        _rankingList.Add(_Score);
+
+        var _rankin = _rankingList.OrderByDescending(r => r).Take(_top5.Length).ToList();
+
+
+
+        for (var i = 0; i < _rankin.Count; i++)
+        {
+
+            _Rankin[i].text = ($"{i + 1}ˆÊ:{_rankin[i]}");
+        }
+
+    }
+}
