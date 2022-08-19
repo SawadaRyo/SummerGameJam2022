4d680bec8a5971fd02f3c928ed777f5c1906c3ae
SR21C <rysawad@gmail.com>
2022-08-19 12:44:14 +0900

    PauseActionスクリプト更新

--- Assets/Adati/Scripts/ProgressManager.cs
+++ Assets/Adati/Scripts/ProgressManager.cs
@@ -18,9 +18,25 @@ public class ProgressManager : SingletonMonoBehaviour<ProgressManager>
 
     /// <summary>プレイヤーとエネミーの距離</summary>
     float _distance;
+    /// <summary>ポーズ直前のスピード</summary>
+    float _beforeSpeed;
+    /// <summary>PauseHanderクラスを格納</summary>
+    PauseHander _pauseHander;
 
     const float OFFSET = 100;
 
+    private void Awake()
+    {
+        _pauseHander = GameObject.FindObjectOfType<PauseHander>();
+    }
+    private void OnEnable()
+    {
+        _pauseHander.PauseAction += PauseRestart;
+    }
+    private void OnDisable()
+    {
+        _pauseHander.PauseAction -= PauseRestart;
+    }
     void Update()
     {
         _distance = ProgressUIManager.Instance.PlayerProgressValue - ProgressUIManager.Instance.EnemyProgressValue;
@@ -29,6 +45,18 @@ public class ProgressManager : SingletonMonoBehaviour<ProgressManager>
         if (TimeManager.Instance.Timer >= _startTime)
         {
             ProgressUIManager.Instance.ChangeEnemyProgress(Time.deltaTime / OFFSET * _speed);
+        }
+    }
+    void PauseRestart(bool isPause)
+    {
+        if (isPause)
+        {
+            _beforeSpeed = _speed;
+            _speed = 0f;
+        }
+        else
+        {
+            _speed = _beforeSpeed;
         }
     }
 }
--- Assets/Sawada/Scripts/GroundMove.cs
+++ Assets/Sawada/Scripts/GroundMove.cs
@@ -6,29 +6,48 @@ public class GroundMove : MonoBehaviour
 {
     [Header("Groundの動くスピード")]
     [SerializeField] float _moveSpeed = 5f;
-    Player _player = default;
+    
 
     float _time = 0f;
+    float _beforeSpeed = 0f;
     Rigidbody2D _rb = default;
+    Player _player = default;
+    PauseHander _hander = default;
 
-    void Start()
+    void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _player = GameObject.FindObjectOfType<Player>();
+        _hander = GameObject.FindObjectOfType<PauseHander>();
+    }
+    private void OnEnable()
+    {
+        _hander.PauseAction += PauseRestart;
+    }
+    private void OnDisable()
+    {
+        _hander.PauseAction -= PauseRestart;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        if (_player.IsWalled()) return;
+        _time += Time.deltaTime;
+        _rb.velocity = new Vector2(-_moveSpeed, 0f);
     }
-    void Move(bool isGame)
+    
+
+    void PauseRestart(bool isPause)
     {
-        if(isGame)
+        if(isPause)
         {
-            if (_player.IsWalled()) return;
-            _time += Time.deltaTime;
-            _rb.velocity = new Vector2(-_moveSpeed, 0f);
+            _beforeSpeed = _moveSpeed;
+            _moveSpeed = 0f;
+        }
+        else
+        {
+            _moveSpeed = _beforeSpeed;
         }
     }
 }
